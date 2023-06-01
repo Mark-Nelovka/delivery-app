afc1c2c01d1c47d7d27048e369846d3e50f43415
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-06-01 17:37:20 +0300

    add form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,5 +1,34 @@
 import * as styles from "./Form.styled";
 
 export const Form = () => {
-  return <div>Form</div>;
+  return (
+    <styles.FormContainer>
+      <styles.Form>
+        <styles.FormLabel htmlFor="name">Name:</styles.FormLabel>
+        <styles.FormInput
+          type="text"
+          id="name"
+          placeholder="Name"
+        ></styles.FormInput>
+        <styles.FormLabel htmlFor="email">Email:</styles.FormLabel>
+        <styles.FormInput
+          type="email"
+          placeholder="Email"
+          id="email"
+        ></styles.FormInput>
+        <styles.FormLabel htmlFor="phone">Phone:</styles.FormLabel>
+        <styles.FormInput
+          type="number"
+          placeholder="Number phone"
+          id="phone"
+        ></styles.FormInput>
+        <styles.FormLabel htmlFor="address">Address:</styles.FormLabel>
+        <styles.FormInput
+          type="text"
+          placeholder="Address"
+          id="address"
+        ></styles.FormInput>
+      </styles.Form>
+    </styles.FormContainer>
+  );
 };
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,3 +1,17 @@
+import { Basket } from "../components/basket/Basket";
+import { Form } from "../components/form/Form";
+import { Container } from "../styles/Container.styled";
+import * as styles from "./ShopsPage.styled";
+
 export default function CartPage() {
-  return <div>Cart page</div>;
+  return (
+    <section>
+      <Container>
+        <styles.ContentContainerCart>
+          <Form />
+          <Basket />
+        </styles.ContentContainerCart>
+      </Container>
+    </section>
+  );
 }
