53a1dea3a7fe6c32429545fae235e479350cf07d
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-06-01 14:56:11 +0300

    add shops list

--- src/components/shopsList/Shops.tsx
+++ src/components/shopsList/Shops.tsx
@@ -1,5 +1,23 @@
 import * as styles from "./Shops.styled";
 
 export const Shops = () => {
-  return <div>Shops</div>;
+  return (
+    <styles.ShopsContainer>
+      <styles.ShopsTitle>Shops: </styles.ShopsTitle>
+      <styles.ShopList>
+        <styles.ShopListItem>MacCdonalds</styles.ShopListItem>
+        <styles.ShopListItem>KFC</styles.ShopListItem>
+        <styles.ShopListItem>Eurazia</styles.ShopListItem>
+        <styles.ShopListItem>Dinner time</styles.ShopListItem>
+        <styles.ShopListItem>
+          Test long textasfsdfsdfsdfsdfsd daasfasfsdfdsfsdfsafasf
+        </styles.ShopListItem>
+        <styles.ShopListItem>Hata</styles.ShopListItem>
+        <styles.ShopListItem>Hata</styles.ShopListItem>{" "}
+        <styles.ShopListItem>Hata</styles.ShopListItem>{" "}
+        <styles.ShopListItem>Hata</styles.ShopListItem>{" "}
+        <styles.ShopListItem>Hata</styles.ShopListItem>
+      </styles.ShopList>
+    </styles.ShopsContainer>
+  );
 };
